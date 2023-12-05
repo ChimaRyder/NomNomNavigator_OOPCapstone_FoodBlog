4479f8b873278a050552b18e296d8c2274323d0d
Frenz R <frenzrpnte@gmail.com>
2023-12-05 15:15:21 +0800

    commented setListData

--- src/RestaurantBlog.java
+++ src/RestaurantBlog.java
@@ -24,7 +24,7 @@ public class RestaurantBlog extends JFrame{
             re.setVisible(true);
 
 
-            RestaurantList.setListData();
+            //RestaurantList.setListData(getRestaurants());
         });
     }
 
