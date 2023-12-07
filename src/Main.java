086b036af7b720f4218f290c991a9c98849199ad
Frenz R <frenzrpnte@gmail.com>
2023-12-07 15:52:29 +0800

    added comment in Main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,18 @@ import java.util.ArrayList;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Main {
+    /*
+    Purpose of this class is to create a singleton pattern for the RestaurantBlog Panel.
+    That way, ArrayList restaurants and ListModel Titles are completely static without affecting
+    the non-static fields of RestaurantBlog.
+
+    Call getInstance() to change any values inside RestaurantBlog from here, such as setting the Model for RestaurantList.
+    Since getInstance() only returns one instance of RestaurantBlog that is static, we can use our static fields here to
+    change any values inside RestaurantBlog.
+
+    This is necessary since GUI components cannot be static and fields like restaurants and Titles need to be global for
+    the restaurantEditor to add new restaurants to the JList.
+     */
     private static ArrayList<restaurant> restaurants;
     private static DefaultListModel<String> Titles;
     private static RestaurantBlog instance;
@@ -10,7 +22,7 @@ public class Main {
     public static void addRestaurant(restaurant e) {
         restaurants.add(e);
         Titles.addElement(e.toString());
-        getInstance().getRestaurantList().setModel(Titles);
+        getInstance().getRestaurantList().setModel(Titles); //
     }
 
     public Main() {
